199c30290b6c2c40ef208f33305c265bd6277c27
Ryan Pennucci <decimalman@gmail.com>
2013-04-26 00:19:56 -0400

    Revert 3.4 FFC fix
    Turns out it's not only useless on 3.0, it causes hangs and/or reboots.

--- drivers/media/video/msm_zsl/sensors/s5k6a3yx.c
+++ drivers/media/video/msm_zsl/sensors/s5k6a3yx.c
@@ -235,15 +235,14 @@ static int s5k6a3yx_sensor_config(void __user *argp)
 {
 	int rc;
 	mutex_lock(s5k6a3yx_s_ctrl.msm_sensor_mutex);
-	return msm_sensor_config(&s5k6a3yx_s_ctrl, argp);
+	rc = msm_sensor_config(&s5k6a3yx_s_ctrl, argp);
 	mutex_unlock(s5k6a3yx_s_ctrl.msm_sensor_mutex);
 	return rc;
 }
 
 static int s5k6a3yx_sensor_open_init(const struct msm_camera_sensor_info *data)
 {
-	s5k6a3yx_s_ctrl.sensordata = data;
-	return 0;
+	return msm_sensor_open_init(&s5k6a3yx_s_ctrl, data);
 }
 
 static int s5k6a3yx_sensor_release(void)
@@ -522,6 +521,8 @@ static struct msm_sensor_fn_t s5k6a3yx_func_tbl = {
 	.sensor_config = s5k6a3yx_sensor_config,
 	.sensor_open_init = s5k6a3yx_sensor_open_init,
 	.sensor_release = s5k6a3yx_sensor_release,
+	.sensor_power_up = msm_sensor_power_up,
+	.sensor_power_down = msm_sensor_power_down,
 	.sensor_probe = msm_sensor_probe,
 };
 
