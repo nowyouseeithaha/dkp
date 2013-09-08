5ac57437393081a882be55081bee4caf68df0397
Ryan Pennucci <decimalman@gmail.com>
2013-09-07 21:46:42 -0400

    Revert "Fix bran-damage in adreno_ringbuffer.c"
    This reverts commit 691b267f94b8e4e914bf886bbd219a12ea8d0b04.

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1018,11 +1018,11 @@ adreno_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 		adreno_dev->drawctxt_active == drawctxt)
 		i = 1;
 
-	cmds = link = kmalloc(sizeof(unsigned int) * (numibs * 3 + 4),
+	cmds = link = kmalloc(sizeof(unsigned int) * ((numibs - i) * 3 + 4),
 				GFP_KERNEL);
-	if (unlikely(!link)) {
+	if (!link) {
 		KGSL_CORE_ERR("kzalloc(%d) failed\n",
-			sizeof(unsigned int) * (numibs * 3 + 4));
+			sizeof(unsigned int) * ((numibs - i) * 3 + 4));
 		return -ENOMEM;
 	}
 
